82617c87ee1869c0367fc30f0d81aa3344c34598
JumarQuinio <j.quiniomesicias@student.reading.ac.uk>
2024-03-15 16:44:22 +0000

    Testb1 try

--- progs/features/TestBugG1.java
+++ progs/features/TestBugG1.java
@@ -1,1 +1,8 @@
-class
+//Bug G1. Allow zero-length lists, such as int ;.
+class TestBugG1 {
+    public static void main(String[] args) {
+        int[] a = new int[0];
+        int[] b = new int[] {};
+        int[] c = {};
+    }
+}
