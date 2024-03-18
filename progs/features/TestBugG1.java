170f1519132b664259777be41d36e975271663a0
JumarQuinio <j.quiniomesicias@student.reading.ac.uk>
2024-03-18 10:39:01 +0000

    bug1 done

--- progs/features/TestBugG1.java
+++ progs/features/TestBugG1.java
@@ -1,8 +1,13 @@
-//Bug G1. Allow zero-length lists, such as int ;.
+// Bug G1. Allow zero-length lists, such as int
 class TestBugG1 {
     public static void main(String[] args) {
-        int[] a = new int[0];
-        int[] b = new int[] {};
-        int[] c = {};
+        System.out.println(new Test().f());
+    }
+}
+class Test {
+    public int f() {
+        // Incorrect initialization of the second variable
+        int x;
+        return 42;
     }
 }
--- progs/features/TestBugG2.java
+++ progs/features/TestBugG2.java
@@ -1,1 +1,16 @@
-class
+//Bug G2. In int x,y; make y a boolean
+class TestBugG2 {
+    public static void main(String[] args) {
+        System.out.println(new Test().f());
+    }
+}
+class Test {
+     public int f() {
+          int x, y;
+          y = true; // Assigning a boolean value to y
+          if (y);
+               return 1;
+          else
+               return 0;
+     }
+}
